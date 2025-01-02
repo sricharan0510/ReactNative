af4c7b5017c3289467c0c817a465ee6b9a79d9f5
Sricharan Illandula <sricharan0504@gmail.com>
2025-01-02 10:31:03 +0530

    Tables

--- Components/Tables.js
+++ Components/Tables.js
@@ -7,13 +7,16 @@ const Tables = () => {
         ['Sricharan', '20', '4223', 'Kothaguden'],
         ['Praveen', '19', '4220', 'Velvein'],
         ['Charann', '21', '4250', 'Hyderabad'],
-    ];
+        ['Karthik', '20', '4254', 'Kakinada'],
+    ]; 
     return (
         <ScrollView horizontal={true}>
             <View style={styles.container}>
-                <Table borderStyle={{ borderWidth: 2, borderColor: 'black' }}>
-                    <Row data={tableHead} style={styles.tableContent} textStyle={{ fontSize: 20, textAlign: 'center', fontWeight: 600 }} />
-                    <Rows data={tableData} style={styles.tableContent} textStyle={{ fontSize: 16, textAlign: 'center' }} />
+                <Table borderStyle={{ borderWidth: 1, borderColor: 'black' }}>
+                    <Row data={tableHead} style={[styles.tableContent, {backgroundColor: '#01987A'}]} textStyle={{ fontSize: 20, textAlign: 'center', fontWeight: '600', color: 'white' }} />
+                    {tableData.map((rowData, index) => (
+                        <Row key={index} data={rowData} style={[styles.tableContent, index%2 && {backgroundColor: '#DBDBDB'} ]} textStyle={{ fontSize: 16, textAlign: 'center' }} />
+                    ))}
                 </Table>
             </View>
         </ScrollView>
