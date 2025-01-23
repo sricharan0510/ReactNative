ec2ba1d8bfbe1d5fc814c6fe88a4717081645c01
Sricharan Illandula <sricharan0504@gmail.com>
2025-01-23 11:37:22 +0530

    Commit

--- App.js
+++ App.js
@@ -8,46 +8,9 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeArea from './Components/SafeArea';
 
 export default function App() {
-  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,];
   return (
     <>
       <View style={styles.container}>
-
-        {/* <Text style={{fontSize: 30}}>Lord Ganesha!</Text>  */}
-
-        {/* <Image source={require('./assets/ganesh.jpg')} style={{ width: 500, height: 500 }} /> */}
-        {/* <Image source={{ uri: "https://www.svtsydney.org/wp-content/uploads/2019/05/Lord_Ganesha-min.jpg" }}
-        style={{ width: 300, height: 300 }} />   */}
-
-        {/* <TextInput placeholder="Enter your name" style={{ padding: 10, borderWidth: 1 }}/> */}
-
-        {/* <FlatList data={data} renderItem={({ item }) => <Text style={{ fontSize: 30 }}>{item}</Text>} /> */}
-
-        {/* <Button title="Click Me" onPress={() => console.log("Button Clicked")} /> */}
-
-        {/* <TouchableOpacity onPress={() => console.log("Button Clicked")}>
-        <Text>Click Me</Text>
-      </TouchableOpacity> */}
-
-        {/* <ActivityIndicator size="large" color="red" /> */}
-
-
-        {/* ============================================================================================== */}
-
-
-        {/* <StudentDetails /> */}
-        {/* <Tables /> */}
-
-        {/* <SafeAreaProvider>
-          <SafeArea />
-        </SafeAreaProvider> */}
-
-        {/* ============================================================================================== */}
-
-        {/* <StatusBar style="auto" /> */}
-
-        {/* ==================================================================================== */}
-
         <Main />
 
       </View>
