d8a2a0041646b8b1c7b763f80b467ba5a1b24f56
Sricharan Illandula <sricharan0504@gmail.com>
2025-01-23 11:48:55 +0530

    Commit

--- Components/Thub/Carousel.js
+++ Components/Thub/Carousel.js
@@ -8,13 +8,13 @@ const Carousel = () => {
 
    return(
     <SafeAreaView>
-      <Text style={{color: "green", fontSize: 10, fontWeight: "bold", marginLeft: 10}}>Digi Wall</Text>
-      <FlatList 
+      <Text style={{color: "green", fontSize: 16, fontWeight: "bold", marginLeft: 10}}>Digi Wall</Text>
+      <FlatList style={{marginTop: 10}}
         data = {Info}
         renderItem={({item})=>(
           <View style={{height:200,width:width,justifyContent:'center',alignItems:'center'}}>
-            <View style={{width:"90%",height:"90%",justifyContent:'center',alignItems:"center", backgroundColor:"lightblue",borderRadius:20}}>
-            <Text style={{fontSize:50}}>{item}</Text>
+            <View style={{width:"90%",height:"90%",justifyContent:'center',alignItems:"center", backgroundColor:"green",borderRadius:20}}>
+            <Text style={{color: 'white', fontSize:20}}>{item}</Text>
             </View>
          </View>
         )}
