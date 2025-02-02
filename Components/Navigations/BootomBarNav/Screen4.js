e76603b530349e92210bcea7b295627dd058210f
Sricharan Illandula <sricharan0504@gmail.com>
2025-02-02 16:16:19 +0530

    Add Screen4 with top tab navigation for Audio and Video Calls

--- Components/Navigations/BootomBarNav/Screen4.js
+++ Components/Navigations/BootomBarNav/Screen4.js
@@ -1,17 +1,40 @@
 import React from 'react'
 import { View, Text, Button } from 'react-native'
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import AudioCalls from './AudioCalls'
 import VideoCalls from './VideoCalls'
 
 function Screen4() {
+    const TopBar = createMaterialTopTabNavigator();
     return (
-        <View>
-            <View>
-
-            </View>
-        </View>
+        <>
+            <TopBar.Navigator
+                screenOptions={{
+                    tabBarActiveTintColor: "green",
+                    tabBarInactiveTintColor: "grey",
+                    tabBarLabelStyle: {
+                        fontSize: 12,
+                        fontWeight: "bold"
+                    },
+                    tabBarIndicatorStyle: {
+                        backgroundColor: "green",
+                        height: 4,
+                    },
+                    swipeEnabled: true,
+                    tabBarStyle: {
+                        backgroundColor: 'white',
+                        height: 80,
+                        alignItems: 'center',
+                    },
+                    lazy: true
+                }}
+            >
+                <TopBar.Screen name="Audio Calls" component={AudioCalls} options={{ tabBarIcon: ({ color }) => <Ionicons name="call" size={25} color={color} /> }} />
+                <TopBar.Screen name="Video Calls" component={VideoCalls} options={{ tabBarIcon: ({ color }) => <FontAwesome name="video-camera" size={25} color={color} /> }} />
+            </TopBar.Navigator>
+        </>
     )
 }
 
