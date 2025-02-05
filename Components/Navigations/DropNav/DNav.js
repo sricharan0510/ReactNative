dd363c57e7c9ec41fd2b046f55cea95d875aa53f
Sricharan Illandula <sricharan0504@gmail.com>
2025-02-05 11:02:16 +0530

    Enhance DNav component by adding drawer icons for Home and Profile screens and adjusting drawer width

--- Components/Navigations/DropNav/DNav.js
+++ Components/Navigations/DropNav/DNav.js
@@ -4,28 +4,42 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import Screen1 from './Screen1'
 import Screen2 from './Screen2'
+import Ionicons from "react-native-vector-icons/Ionicons"
 
 function DNav() {
     const DropBar = createDrawerNavigator();
     return (
-        <View style={{flex: 1}}>
+        <View style={{ flex: 1 }}>
             <NavigationContainer>
-                <DropBar.Navigator 
-                screenOptions={{
-                    drawerStyle: {
-                        width: 220,
-                        backgroundColor : "white",
-                    },
-                    drawerLabelStyle : {
-                        fontSize: 16 
-                    },
-                    drawerActiveBackgroundColor: "green",
-                    drawerInactiveTintColor: "black",
-                    drawerActiveTintColor: "white"
-                }}
+                <DropBar.Navigator
+                    screenOptions={{
+                        drawerStyle: {
+                            width: 250,
+                            backgroundColor: "white",
+                        },
+                        drawerActiveBackgroundColor: "green",
+                        drawerInactiveTintColor: "black",
+                        drawerActiveTintColor: "white"
+                    }}
                 >
-                    <DropBar.Screen name='Home' component={Screen1}/>
-                    <DropBar.Screen name='Profile' component={Screen2}/>
+                    <DropBar.Screen
+                        name="Home"
+                        component={Screen1}
+                        options={{
+                            drawerIcon: ({ focused, color, size }) => (
+                                <Ionicons name={focused ? "home" : "home-outline"} size={size} color={color} />
+                            ),
+                        }}
+                    />
+                    <DropBar.Screen
+                        name="Profile"
+                        component={Screen2}
+                        options={{
+                            drawerIcon: ({ focused, color, size }) => (
+                                <Ionicons name={focused ? "person" : "person-outline"} size={size} color={color} />
+                            ),
+                        }}
+                    />
                 </DropBar.Navigator>
             </NavigationContainer>
         </View>
